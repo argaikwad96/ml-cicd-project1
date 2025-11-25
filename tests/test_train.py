7f3850ee0046aca184928e8f486df8db72b8c4bb
Akshay Gaikwad <akshaygcodenera@gmail.com>
2025-11-25 17:25:07 +0530

    Merge remote changes

--- tests/test_train.py
+++ tests/test_train.py
@@ -5,7 +5,7 @@ from src.train import train_model
 def test_model_training():
     train_model()
     
-    os.path.exist("models/model.pkl"),"Model file not found"
+    os.path.exists("models/model.pkl"),"Model file not found"
     model = joblib.load("models/model.pkl")
     assert hasattr(model,"predict"),"Model does nothave a predict method"
 
